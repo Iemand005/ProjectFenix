2dbf7230fad8ec40285403aab9ca54b3e9f0e4a3
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-27 16:41:30 +0200

    I should remove this override thing too

--- cxx_override.cmake
+++ cxx_override.cmake
@@ -1,10 +1,12 @@
-# Nuclear override: replace CMake's default MSVC library list with pure legacy Win32 libs
-# This strips out onecore, onecoreuap, mincore, and all api-ms-win-* umbrella libs.
-set(CMAKE_CXX_STANDARD_LIBRARIES
-    "kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib"
-    CACHE STRING "" FORCE
-)
-set(CMAKE_C_STANDARD_LIBRARIES
-    "kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib"
-    CACHE STRING "" FORCE
-)
+if(WIN32 AND NOT EMSCRIPTEN)
+    # Nuclear override: replace CMake's default MSVC library list with pure legacy Win32 libs
+    # This strips out onecore, onecoreuap, mincore, and all api-ms-win-* umbrella libs.
+    set(CMAKE_CXX_STANDARD_LIBRARIES
+        "kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib"
+        CACHE STRING "" FORCE
+    )
+    set(CMAKE_C_STANDARD_LIBRARIES
+        "kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib"
+        CACHE STRING "" FORCE
+    )
+endif()
