8d8c78031322381154933bd7366e42a42241541d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-28 02:48:02 +0200

    camek funks better functino for the things

--- EmscriptenConfig.cmake
+++ EmscriptenConfig.cmake
@@ -3,45 +3,50 @@ if(TARGET emscripten_wasm_settings)
     return()
 endif()
 
-if(EMSCRIPTEN)
-    # Create a dummy "interface" target to hold all our shared compiler/linker settings
-    add_library(emscripten_wasm_settings INTERFACE)
 
-    # 1. Shared Compile Options
-    target_compile_options(emscripten_wasm_settings INTERFACE
+function(setup_emscripten_target target_name)
+    if(NOT EMSCRIPTEN)
+        return()
+    endif()
+
+    set(oneValueArgs RESOURCE_DIR)
+    set(multiValueArgs EXTRA_LINK_OPTIONS)
+    cmake_parse_arguments(ARG "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    target_compile_options(${target_name} PRIVATE
         -g
         -fdebug-compilation-dir="${CMAKE_SOURCE_DIR}"
     )
 
-    # 2. Shared Link Options (Combining -g flags and all your -s Emscripten switches)
-    target_link_options(emscripten_wasm_settings INTERFACE
+    target_link_options(${target_name} PRIVATE
         -g
         -gsource-map
-        "-sUSE_WEBGL2=1"
-        "-sFULL_ES3=1"
-        "-sGL_ENABLE_GET_PROC_ADDRESS=1"
-        "-sALLOW_MEMORY_GROWTH=1"
-        "-sINITIAL_MEMORY=64MB"
-        "-sNO_EXIT_RUNTIME=1"
-        "-sEXIT_RUNTIME=0"
-        "-sMAX_WEBGL_VERSION=2"
-        "-sMIN_WEBGL_VERSION=2"
-        "-sFORCE_FILESYSTEM=1"
-        "-sEXPORTED_RUNTIME_METHODS=FS,ccall,cwrap"
-        "-sEXPORTED_FUNCTIONS=_main,_emscripten_file_dialog_callback,_malloc,_free"
+        -sUSE_WEBGL2=1
+        -sFULL_ES3=1
+        -sGL_ENABLE_GET_PROC_ADDRESS=1
+        -sALLOW_MEMORY_GROWTH=1
+        -sINITIAL_MEMORY=64MB
+        -sNO_EXIT_RUNTIME=1
+        -sEXIT_RUNTIME=0
+        -sMAX_WEBGL_VERSION=2
+        -sMIN_WEBGL_VERSION=2
+        -sFORCE_FILESYSTEM=1
+        -sEXPORTED_RUNTIME_METHODS=FS,ccall,cwrap
+        -sEXPORTED_FUNCTIONS=_main,_emscripten_file_dialog_callback,_malloc,_free
     )
-endif()
 
-# Reusable function to handle target-specific assets and extensions
-function(configure_emscripten_target TARGET_NAME RESOURCE_DIR)
-    if(EMSCRIPTEN)
-        # Force the output to be an HTML file instead of .js or .wasm
-        set_target_properties(${TARGET_NAME} PROPERTIES SUFFIX ".html")
-
-        # Convert backslashes to forward slashes for Emscripten file packager on Windows
-        file(TO_CMAKE_PATH "${RESOURCE_DIR}" _NORM_RES_DIR)
-        target_link_options(${TARGET_NAME} PRIVATE 
-            "--preload-file ${_NORM_RES_DIR}@resources"
+    if(ARG_RESOURCE_DIR)
+        if(NOT EXISTS "${ARG_RESOURCE_DIR}")
+            message(WARNING "setup_emscripten_target(${target_name}): RESOURCE_DIR '${ARG_RESOURCE_DIR}' does not exist")
+        endif()
+        target_link_options(${target_name} PRIVATE
+            "--preload-file=${ARG_RESOURCE_DIR}@resources"
         )
     endif()
+
+    if(ARG_EXTRA_LINK_OPTIONS)
+        target_link_options(${target_name} PRIVATE ${ARG_EXTRA_LINK_OPTIONS})
+    endif()
+
+    set_target_properties(${target_name} PROPERTIES SUFFIX ".html")
 endfunction()
