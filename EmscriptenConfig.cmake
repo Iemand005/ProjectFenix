c08b3407136c60f879bfc78534779dc3e26745b6
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-28 04:24:47 +0200

    Release preset small!!

--- EmscriptenConfig.cmake
+++ EmscriptenConfig.cmake
@@ -13,19 +13,29 @@ function(setup_emscripten_target target_name)
     set(multiValueArgs EXTRA_LINK_OPTIONS)
     cmake_parse_arguments(ARG "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
+    # Debug: symbols + source maps
     target_compile_options(${target_name} PRIVATE
-        -g
-        -fdebug-compilation-dir="${CMAKE_SOURCE_DIR}"
+        $<$<CONFIG:Debug>:-g -fdebug-compilation-dir="${CMAKE_SOURCE_DIR}">
+    )
+    target_link_options(${target_name} PRIVATE
+        $<$<CONFIG:Debug>:-g -gsource-map>
     )
 
+    # Release: maximum size optimization, no debug info
+    target_compile_options(${target_name} PRIVATE
+        $<$<CONFIG:Release>:-Oz -flto>
+    )
     target_link_options(${target_name} PRIVATE
-        -g
-        -gsource-map
+        $<$<CONFIG:Release>:-Oz -flto --closure 1 -sASSERTIONS=0>
+    )
+
+    # Common link flags
+    target_link_options(${target_name} PRIVATE
         -sUSE_WEBGL2=1
         -sFULL_ES3=1
         -sGL_ENABLE_GET_PROC_ADDRESS=1
         -sALLOW_MEMORY_GROWTH=1
-        -sINITIAL_MEMORY=64MB
+        -sINITIAL_MEMORY=128MB
         -sNO_EXIT_RUNTIME=1
         -sEXIT_RUNTIME=0
         -sMAX_WEBGL_VERSION=2
@@ -39,8 +49,9 @@ function(setup_emscripten_target target_name)
         if(NOT EXISTS "${ARG_RESOURCE_DIR}")
             message(WARNING "setup_emscripten_target(${target_name}): RESOURCE_DIR '${ARG_RESOURCE_DIR}' does not exist")
         endif()
+        file(TO_CMAKE_PATH "${ARG_RESOURCE_DIR}" _res_cmake_path)
         target_link_options(${target_name} PRIVATE
-            "--preload-file=${ARG_RESOURCE_DIR}@resources"
+            "--preload-file=${_res_cmake_path}@resources"
         )
     endif()
 
